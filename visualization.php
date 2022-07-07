8cf84d954fdeb35e0baf7d8e624d915da7b93b81
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-07-07 11:48:21 +0100

    Upload a new chart example.

--- visualization.php
+++ visualization.php
@@ -139,7 +139,7 @@
 		</div>
 		<!-- banner end -->
 
-		<div id="main" style="width: 800px; height:500px; float:center; "></div>
+		<div id="main" style="width: 100%; height:500px; float:center; "></div>
     	<script type="text/javascript">
         var myChart = echarts.init(document.getElementById('main'));
 
@@ -163,91 +163,86 @@
     	</script>
 
 
-		<!-- section start -->
-		<!-- ================ -->
-		<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h1 id="about" class="title text-center">About Imperial College <span>Sustainability </span></h1>
-						<p class="lead text-center">The project aims to improve the sustainability of Imperial College London.</p>
-						<div class="space"></div>
-						<div class="row">
-							<div class="col-md-6">
-								<img src="images/ic_logo.png" alt="">
-								<div class="space"></div>
-							</div>
-							<div class="col-md-6">
-								<p>The project aims to improve the sustainability of Imperial College London. Through visualization, data analysis and other methods to actually investigate the use of different energy resources in Imperial College London, and try to maximize the utilization of resources.</p>
-								
-								<ul class="list-unstyled">
-									<li><i class="fa fa-caret-right pr-10 text-colored"></i> Water</li>
-									<li><i class="fa fa-caret-right pr-10 text-colored"></i> Electricity</li>
-									<li><i class="fa fa-caret-right pr-10 text-colored"></i> Gas</li>
-									<li><i class="fa fa-caret-right pr-10 text-colored"></i> Meal</li>
-								</ul>
-							</div>
-						</div>
-						<div class="space"></div>
-						<h2>Description of energy</h2>
-						<div class="row">
-							<div class="col-md-6">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi adipisci illo, voluptatum ipsam fuga error commodi architecto, laudantium culpa tenetur at id, beatae placeat deserunt iure quas voluptas fugit eveniet.</p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo ducimus explicabo quibusdam temporibus deserunt doloremque pariatur ea, animi a. Delectus similique atque eligendi, enim vel reiciendis deleniti neque aliquid, sit?</p>
-							</div>
-							<div class="col-md-6">
-								<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-									<div class="panel panel-default">
-										<div class="panel-heading" role="tab" id="headingOne">
-											<h4 class="panel-title">
-												<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-													Electricity
-												</a>
-											</h4>
-										</div>
-										<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-											<div class="panel-body">
-												Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher.
-											</div>
-										</div>
-									</div>
-									<div class="panel panel-default">
-										<div class="panel-heading" role="tab" id="headingTwo">
-											<h4 class="panel-title">
-												<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-													Water
-												</a>
-											</h4>
-										</div>
-										<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-											<div class="panel-body">
-												Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-											</div>
-										</div>
-									</div>
-									<div class="panel panel-default">
-										<div class="panel-heading" role="tab" id="headingThree">
-											<h4 class="panel-title">
-												<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-													Gas
-												</a>
-											</h4>
-										</div>
-										<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-											<div class="panel-body">
-												Richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic.
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- section end -->
+		<div id="datechart" style="width: 100%; height:500px; float:center; "></div>
+    	<script type="text/javascript">
 
+        var myChart = echarts.init(document.getElementById('datechart'));
+    	let base = +new Date(2019, 9, 3);
+		let oneDay = 24 * 3600 * 1000;
+		let date = [];
+		let data = [Math.random() * 200];
+		for (let i = 1; i < 1000; i++) {
+		  var now = new Date((base += oneDay));
+		  date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+		  data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
+		}
+		option = {
+		  tooltip: {
+		    trigger: 'axis',
+		    position: function (pt) {
+		      return [pt[0], '10%'];
+		    }
+		  },
+		  title: {
+		    left: 'center',
+		    text: 'Large Area Chart'
+		  },
+		  toolbox: {
+		    feature: {
+		      dataZoom: {
+		        yAxisIndex: 'none'
+		      },
+		      restore: {},
+		      saveAsImage: {}
+		    }
+		  },
+		  xAxis: {
+		    type: 'category',
+		    boundaryGap: false,
+		    data: date
+		  },
+		  yAxis: {
+		    type: 'value',
+		    boundaryGap: [0, '100%']
+		  },
+		  dataZoom: [
+		    {
+		      type: 'inside',
+		      start: 0,
+		      end: 10
+		    },
+		    {
+		      start: 0,
+		      end: 10
+		    }
+		  ],
+		  series: [
+		    {
+		      name: 'Fake Data',
+		      type: 'line',
+		      symbol: 'none',
+		      sampling: 'lttb',
+		      itemStyle: {
+		        color: 'rgb(255, 70, 131)'
+		      },
+		      areaStyle: {
+		        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+		          {
+		            offset: 0,
+		            color: 'rgb(255, 158, 68)'
+		          },
+		          {
+		            offset: 1,
+		            color: 'rgb(255, 70, 131)'
+		          }
+		        ])
+		      },
+		      data: data
+		    }
+		  ]
+		};
+
+		</script>
 
 		<!-- footer start -->
 		<!-- ================ -->
