f9516bada776b35c408283601afccfd4cc840497
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-07-07 16:02:24 +0100

    Update charts.

--- visualization.php
+++ visualization.php
@@ -139,6 +139,111 @@
 		</div>
 		<!-- banner end -->
 
+		<div id="elecWeekChart" style="width: 100%; height:500px; float:center; "></div>
+    	<script type="text/javascript">
+        var elecWeekChart = echarts.init(document.getElementById('elecWeekChart'));
+
+        option = {
+		  title: {
+		    text: 'Electricity Week Chart'
+		  },
+		  tooltip: {
+		    trigger: 'axis',
+		    axisPointer: {
+		      type: 'cross',
+		      label: {
+		        backgroundColor: '#6a7985'
+		      }
+		    }
+		  },
+		  legend: {
+		    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+		  },
+		  toolbox: {
+		    feature: {
+		      saveAsImage: {}
+		    }
+		  },
+		  grid: {
+		    left: '3%',
+		    right: '4%',
+		    bottom: '3%',
+		    containLabel: true
+		  },
+		  xAxis: [
+		    {
+		      type: 'category',
+		      boundaryGap: false,
+		      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+		    }
+		  ],
+		  yAxis: [
+		    {
+		      type: 'value'
+		    }
+		  ],
+		  series: [
+		    {
+		      name: 'Email',
+		      type: 'line',
+		      stack: 'Total',
+		      areaStyle: {},
+		      emphasis: {
+		        focus: 'series'
+		      },
+		      data: [120, 132, 101, 134, 90, 230, 210]
+		    },
+		    {
+		      name: 'Union Ads',
+		      type: 'line',
+		      stack: 'Total',
+		      areaStyle: {},
+		      emphasis: {
+		        focus: 'series'
+		      },
+		      data: [220, 182, 191, 234, 290, 330, 310]
+		    },
+		    {
+		      name: 'Video Ads',
+		      type: 'line',
+		      stack: 'Total',
+		      areaStyle: {},
+		      emphasis: {
+		        focus: 'series'
+		      },
+		      data: [150, 232, 201, 154, 190, 330, 410]
+		    },
+		    {
+		      name: 'Direct',
+		      type: 'line',
+		      stack: 'Total',
+		      areaStyle: {},
+		      emphasis: {
+		        focus: 'series'
+		      },
+		      data: [320, 332, 301, 334, 390, 330, 320]
+		    },
+		    {
+		      name: 'Search Engine',
+		      type: 'line',
+		      stack: 'Total',
+		      label: {
+		        show: true,
+		        position: 'top'
+		      },
+		      areaStyle: {},
+		      emphasis: {
+		        focus: 'series'
+		      },
+		      data: [820, 932, 901, 934, 1290, 1330, 1320]
+		    }
+		  ]
+		};
+        elecWeekChart.setOption(option);
+
+    	</script>
+
+
 		<div id="elecDateChart" style="width: 100%; height:500px; float:center; "></div>
     	<script type="text/javascript">
 
@@ -240,7 +345,7 @@
 		            color: 'rgb(0, 191, 255)'
 		          }
 		        ]),
-		        opacity: 0.5
+		        opacity: 0.8
 		      },
 		      data: data_2020
 		    }
