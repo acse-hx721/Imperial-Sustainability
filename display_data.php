46abd5384f9916022c6c4504b1f71ce273ac195f
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-07-19 23:12:47 +0100

    Update read file function.

--- display_data.php
+++ display_data.php
@@ -786,7 +786,7 @@
 
 		<script type="text/javascript">
 		var elec_data;
-		var elec_data_file = new File(["elec_data_file"], "data/all_elec_data.csv"
+		var elec_data_file = new File(["elec_data_file"], "data/all_elec_data.csv")
 
 		Papa.parse(elec_data_file, {
 		  header: true,
