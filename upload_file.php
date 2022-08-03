8c09e58e5af8ce3141f07ff111f744b2c0f83eb7
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-08-03 16:21:41 +0100

    Update php to upload new file.

--- upload_file.php
+++ upload_file.php
@@ -1,21 +1,16 @@
 <?php
-// 允许上传的图片后缀
-$allowedExts = array("gif", "jpeg", "jpg", "png");
+// 允许上传的文件后缀
+$allowedExts = array("csv");
 $temp = explode(".", $_FILES["file"]["name"]);
 echo $_FILES["file"]["size"];
 $extension = end($temp);     // 获取文件后缀名
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 204800)   // 小于 200 kb
+if ((($_FILES["file"]["type"] == "text/csv")
+&& ($_FILES["file"]["size"] < 20480000)   // 小于 20000 kb
 && in_array($extension, $allowedExts))
 {
 	if ($_FILES["file"]["error"] > 0)
 	{
-		echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+		echo "错误：" . $_FILES["file"]["error"] . "<br>";
 	}
 	else
 	{
@@ -26,20 +21,20 @@ if ((($_FILES["file"]["type"] == "image/gif")
 		
 		// 判断当期目录下的 upload 目录是否存在该文件
 		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-		if (file_exists("upload/" . $_FILES["file"]["name"]))
-		{
-			echo $_FILES["file"]["name"] . " 文件已经存在。 ";
-		}
-		else
-		{
-			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-			move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-			echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
-		}
+		// if (file_exists("data/" . $_FILES["file"]["name"]))
+		// {
+		// 	echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+		// }
+		// else
+		// {
+		// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+		move_uploaded_file($_FILES["file"]["tmp_name"], "data/" . $_FILES["file"]["name"]);
+		echo "文件存储在: " . "data/" . $_FILES["file"]["name"];
+		// }
 	}
 }
 else
 {
-	echo "非法的文件格式";
+	echo "Illegal file format.";
 }
 ?>
