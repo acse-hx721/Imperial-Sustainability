8e69cd298a4882bcbc4afd0aadbefe80dbf95ec7
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-08-03 16:35:54 +0100

    Update php to upload new file.

--- upload_file.php
+++ upload_file.php
@@ -1,10 +1,11 @@
 <?php
 // 允许上传的文件后缀
-$allowedExts = array("csv", "xls");
+$allowedExts = array("csv");
 $temp = explode(".", $_FILES["file"]["name"]);
 echo $_FILES["file"]["size"];
 $extension = end($temp);     // 获取文件后缀名
-if ($_FILES["file"]["type"] == "text/csv")   // 小于 20000 kb
+if (($_FILES["file"]["size"] < 20480000)   // 小于 20000 kb
+&& in_array($extension, $allowedExts))
 {
 	if ($_FILES["file"]["error"] > 0)
 	{
