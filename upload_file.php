4e5a060e08557245471649245e0ee0c37f39ec5b
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-08-03 17:49:17 +0100

    Update code to upload new file.

--- upload_file.php
+++ upload_file.php
@@ -9,14 +9,14 @@ $extension = end($temp);     // 获取文件后缀名
 // {
 if ($_FILES["file"]["error"] > 0)
 {
-	echo "错误：" . $_FILES["file"]["error"] . "<br>";
+	echo "Error：" . $_FILES["file"]["error"] . "<br>";
 }
 else
 {
-	echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-	echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-	echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-	echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
+	echo "File Name: " . $_FILES["file"]["name"] . "<br>";
+	echo "File Type: " . $_FILES["file"]["type"] . "<br>";
+	echo "File Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+	echo "Temporary Storage Location: " . $_FILES["file"]["tmp_name"] . "<br>";
 	
 	// 判断当期目录下的 upload 目录是否存在该文件
 	// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
@@ -28,7 +28,7 @@ else
 	// {
 	// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
 	move_uploaded_file($_FILES["file"]["tmp_name"], "data/" . $_FILES["file"]["name"]);
-	echo "文件存储在: " . "data/" . $_FILES["file"]["name"];
+	echo "Files are stored in: " . "data/" . $_FILES["file"]["name"];
 	// }
 }
 // }
@@ -37,3 +37,9 @@ else
 // 	echo "Illegal file format.";
 // }
 ?>
+
+<script type="text/javascript">
+
+document.location="index.php";
+
+</script>
