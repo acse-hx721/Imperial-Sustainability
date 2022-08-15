4ee682f914467874b43ffb4b824eb0c6ae715ed0
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-08-15 17:43:48 +0100

    Modified gas part.Debug.

--- display_data.php
+++ display_data.php
@@ -1369,7 +1369,7 @@
 
 				float_data = float_data.toFixed(0);
 				float_data = parseFloat(float_data);
-				if (float_data > Math.abs(10000 * last_data) || float_data > threshold || float_data < -threshold){
+				if (float_data > Math.abs(100000 * last_data) || float_data > threshold || float_data < -threshold){
 					float_data = last_data;
 				}
 				if (float_data != 0){
