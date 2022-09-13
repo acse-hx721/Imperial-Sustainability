85fc34c3431cba5a6db1bf006d487937c7907e70
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-09-14 00:16:15 +0800

    Debug.

--- display_data.php
+++ display_data.php
@@ -1699,7 +1699,7 @@
 			var datetime1 = this_year_week_datetime;
 			datetime1 = getDay(datetime1, 1);
 			for (var i = 0; i < 7; i++) {
-				var month = datetime1.getMonth() + 2;
+				var month = datetime1.getMonth() + 1;
 				var day = datetime1.getDate() + 1;
 				var datetime_str = day + '/' + month + '/' + datetime1.getFullYear();
 				var week = datetime1.getDay();
