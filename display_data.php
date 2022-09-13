4db6e40d2d164a5b79ed014d47c9783a8c353e15
acse-hx721 <hongcheng.xie21@imperial.ac.uk>
2022-09-13 23:51:46 +0800

    Debug.

--- display_data.php
+++ display_data.php
@@ -1697,9 +1697,10 @@
 
 			// Draw the heat map
 			var datetime1 = this_year_week_datetime;
+			datetime1 = getDay(datetime1, 1);
 			for (var i = 0; i < 7; i++) {
-				var month = datetime1.getMonth() + 1;
-				var day = datetime1.getDate() + 1;
+				var month = datetime1.getMonth();
+				var day = datetime1.getDate();
 				var datetime_str = day + '/' + month + '/' + datetime1.getFullYear();
 				var week = datetime1.getDay();
 				if (week == 0) {  
